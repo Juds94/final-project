43d076e125b2f934fa59b9509f563f0b3a9eaa06
Judit Angulo <judith1518@gmail.com>
2022-02-28 13:18:09 +0100

    conflict result bcrypt

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,9 +1,4 @@
 const router = require("express").Router();
-const Places = require('./../models/Place.model')
-
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-});
 
 router.use("/places", (req, res, next) => {
   res.json("holita")
