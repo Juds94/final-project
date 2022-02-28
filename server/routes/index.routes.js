11dee3302ed5889bed75c7ab7826b5788b6d138a
Gustavo del Llano <gustavodelllano@hotmail.es>
2022-02-28 17:01:01 +0100

    crud pitches

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -4,7 +4,10 @@ router.use("/places", (req, res, next) => {
   res.json("holita")
 })
 
-module.exports = router;
+router.use("/pitches",require("./pitches.routes"))
+
 router.use("/auth", require("./auth.routes"))
+
+// router.use("/user", require("./user.routes"))
 
 module.exports = router
