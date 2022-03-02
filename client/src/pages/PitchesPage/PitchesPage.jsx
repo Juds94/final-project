b93921061ffea26db99324aa45ab999438b7900d
Gustavo del Llano <gustavodelllano@hotmail.es>
2022-03-02 15:37:45 +0100

    enrooted

---
+++ client/src/pages/PitchesPage/PitchesPage.jsx
@@ -0,0 +1,10 @@
+const PitchesPage = () => {
+
+    return(
+        <>
+        <h1>pitchesPage</h1>
+        </>
+    )
+}
+
+export default PitchesPage
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,11 +1,24 @@
 import { Routes, Route } from 'react-router-dom'
 import IndexPage from '../pages/IndexPage/IndexPage'
+import LoginPage from '../pages/LogInPage/LoginPage'
+import PitchesPage from '../pages/PitchesPage/PitchesPage'
+import PlacesPage from '../pages/PlacesPage/PlacesPage'
+import ProfilePage from '../pages/ProfilePage/ProfilePage'
+import SignupPage from '../pages/SignupPage/SignupPage'
+import UsersPage from '../pages/UsersPage/UsersPage'
+
 const AppRoutes = () => {
 
     return(
         <Routes>
 
             <Route path="/" element={<IndexPage/>}/>
+            <Route path="/escuelas" element={<PlacesPage/>}/>
+            <Route path="/vias" element={<PitchesPage/>}/>
+            <Route path="/registro" element={<SignupPage/>}/>
+            <Route path="/inicio-sesion" element={<LoginPage/>}/>
+            <Route path="/usuarios" element={<UsersPage/>}/>
+            <Route path="/perfil" element={<ProfilePage/>}/>
 
         </Routes>
     )
